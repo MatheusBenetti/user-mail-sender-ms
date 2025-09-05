cb5de9d040596a05a1f0322d8b5c32a6179136f7
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-05 12:02:44 -0300

    Create user repository

---
+++ user/src/main/java/com/totex/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.totex.user.repository;
+
+import com.totex.user.domain.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
