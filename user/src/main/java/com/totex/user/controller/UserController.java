48361ba7c23f15da327e5db7b0c837c45dcd7943
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-05 12:02:21 -0300

    Create user controller

---
+++ user/src/main/java/com/totex/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.totex.user.controller;
+
+import com.totex.user.domain.UserModel;
+import com.totex.user.dto.UserDto;
+import com.totex.user.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<UserModel> createUser(@RequestBody UserDto userDto) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
+    }
+}
